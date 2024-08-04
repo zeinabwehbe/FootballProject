815b481e65e0ad188e06699877d5f2adde3117f6
Zaynab Wehbe <zaynab-wehbe@hotmail.com>
2024-08-04 18:15:34 +0300

    Structured File

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
+const playerRoutes = require('./players/playerRoutes');
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const playerRoutes = require('./players/playerRoutes');
+app.use(express.json());
 app.use('/api/players', playerRoutes);
+
+
 
 module.exports = app;
